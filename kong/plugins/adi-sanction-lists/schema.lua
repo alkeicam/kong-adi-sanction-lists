d22efe2f7837cf33dca925d2181d1aa1430d3b35
Maciej Grula <maciej.grula@execon.pl>
2025-01-08 19:53:37 +0100

    ready to publish

--- kong/plugins/adi-sanction-lists/schema.lua
+++ kong/plugins/adi-sanction-lists/schema.lua
@@ -10,9 +10,9 @@ local schema = {
             fields = {
                 { validation_url = typedefs.url {
                     required = true,
-                    default = "https://a9f7-91-189-27-101.ngrok-free.app/adi-in-proxy/8/v1/list/scan", -- Default URL
+                    default = "https://api-di.abee.cloud:3443/adi-in-proxy/8/v1/list/scan", -- Default URL
                 } },
-                { api_key = { type = "string", required = true, default = "wp8bgx4a" } },
+                { api_key = { type = "string", required = true, default = "162rbo9g" } },
                 { api_timeout = { type = "integer", default = 1000, required = true } },  -- Timeout in ms
             },
         } },
